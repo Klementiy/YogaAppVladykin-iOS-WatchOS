6a760f3e7fd2cd01a6c9e0ac2fbdbc4379ac3c4d
Klementiy <74532062+Klementiy@users.noreply.github.com>
2020-12-10 00:09:51 +0300

    Ver.1

---
+++ YogaAppVladykin WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,37 @@
+//
+//  InterfaceController.swift
+//  YogaAppVladykin WatchKit Extension
+//
+//  Created by user on 08/12/2020.
+//
+
+import WatchKit
+import Foundation
+import WatchConnectivity
+
+class InterfaceController: WKInterfaceController, WCSessionDelegate {
+    
+    var wcSession: WCSession!
+    
+    @IBOutlet weak var name: WKInterfaceLabel!
+    @IBOutlet weak var image: WKInterfaceImage!
+    @IBOutlet weak var timer: WKInterfaceTimer!
+
+    override func awake(withContext context: Any?) {
+        wcSession = WCSession.default
+        wcSession.delegate = self
+        wcSession.activate()
+    }
+    
+    func session(_ session: WCSession, didReceiveApplicationContext applicationContext: [String : Any]) {
+        let message = applicationContext["name"] as? String
+        let time = applicationContext["time"] as? Double
+        name.setText(message)
+        timer.setDate(NSDate(timeIntervalSinceNow: time!) as Date)
+        timer.start()
+    }
+    
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+    }
+
+}
